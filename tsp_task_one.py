23461e8568b09ca22fc14c54fd9d99d442a0e4f6
BatmanWVegas <filip.oliwa@gmail.com>
2018-10-27 22:26:22 +0200

    Rnage BUG Fixed, Test data added

--- tsp_task_one.py
+++ tsp_task_one.py
@@ -1,36 +1,42 @@
 from bs4 import BeautifulSoup
 from math import inf
+import timeit, time
 import itertools
 
-file_dict = {
-    'symetric': {
-        '14': 'symetric_data/burma14.xml',
-        '48': 'symetric_data/gr48.xml',
-    },
-    'asymetric': {
-        '17': 'asymetric_data/br17.xml',
-        '33': 'asymetric_data/ftv33.xml',
-    }
-}
 
-
-class Tsp:
-
-    def __init__(self, file):
-        self.file = file
-        self.lowest_route = inf
-        self.lowest_cost = []
-        self.number_of_cities = 0
+class BaseParser():
+    def __init__(self, data_type, number_of_nodes, test):
+        self.file_dict = {
+            'symetric': {
+                '14': 'symetric_data/burma14.xml',
+                '48': 'symetric_data/gr48.xml',
+            },
+            'asymetric': {
+                '17': 'asymetric_data/br17.xml',
+                '33': 'asymetric_data/ftv33.xml',
+            }
+        }
         self.adjacency_matrix = []
+        self.test_matrix = [[0, 3, 5, 7],
+                            [3, 0, 4, 9],
+                            [5, 4, 0, 1],
+                            [7, 9, 1, 0]]
+        if test:
+            self.adjacency_matrix = self.test_matrix
+            self.display_matrix()
+        else:
+            self.prepare_adjacency_matrix(self.file_dict[data_type][number_of_nodes])
+            self.display_matrix()
 
-    def prepare_adjacency_matrix(self):
-        with open(self.file) as file_hendler:
+    def prepare_adjacency_matrix(self, file):
+        with open(file) as file_hendler:
             content = BeautifulSoup(file_hendler, "xml")
         cities = content.find_all('vertex')
-        self.number_of_cities = len(cities[0].find_all('edge'))+1     # +1 to avoid OutOfRange error
-        print(self.number_of_cities)
+        # +1 do liczby miast zeby uninac bledu OutOfRange error.
+        # W plikach XML brakuje indeksu miasta w ktorym aktualnie sie znajdujemy
+        number_of_cities = len(cities[0].find_all('edge'))+1
         for i in range(len(cities)):
-            self.adjacency_matrix.append([0]*self.number_of_cities)
+            self.adjacency_matrix.append([0]*number_of_cities)
             roads = cities[i].find_all('edge')
             for road in roads:
                 self.adjacency_matrix[i][int(road.text)] = int(float(road.get("cost")))
@@ -39,33 +45,62 @@ class Tsp:
         for row in self.adjacency_matrix:
             print(row)
 
-    def prepare_dict_of_nodes(self, file):
-        pass
+
+class Tsp(BaseParser):
+    def __init__(self, data_type='symetric', number_of_nodes = '14', test=False):
+        BaseParser.__init__(self,data_type, number_of_nodes, test)
+        self.time_passed = None
+        self.lowest_cost = inf
+        self.best_route = []
+        self.run()
+
+    def run(self):
+        self.lowest_cost, self.best_route = self.calculate_symetric_cost(self.adjacency_matrix)
+        self.print_results()
+
+    def print_results(self):
+        print("=================== BEST ROUTE =========================")
+        print(self.best_route, ' --->', self.lowest_cost)
+        print("======= OBLICZENIA ZAJELY ", self.time_passed, " ============")
+
+    # Dla symetrycznych nie trzba przeszukiwac wszystkich
+    # mozliwych permutacji: [1,2,3] = [2,3,1] = [3,1,2]
+    def calculate_symetric_cost(self, data_matrix):
+        import ipdb; ipdb.set_trace()
+        cost = inf
+        best_route = []
+        routes = itertools.permutations(list(range(1, len(data_matrix[0]))))
+        for route in routes:
+            route = list(route)
+            route.insert(0, 0)
+            new_travel_cost = 0
+            for i in range(len(route)-1):
+                new_travel_cost += data_matrix[route[i]][route[i+1]]
+            new_travel_cost += data_matrix[route[len(route)-1]][0]
+            if new_travel_cost < cost:
+                cost, best_route = new_travel_cost, route
+        return cost, best_route
+
+    # Dla niesymetrycznych danych przeszukujemy caly zestaw permutacji
+    def calculate_asymetric_cost(self, data_matrix):
+        cost = inf
+        best_route = []
+        routes = itertools.permutations(list(range(len(data_matrix[0]))))
+        for route in routes:
+            new_travel_cost = 0
+            for i in range(len(route) - 1):
+                new_travel_cost += data_matrix[route[i]][route[i + 1]]
+            new_travel_cost += data_matrix[route[len(route) - 1]][0]
+            if new_travel_cost < cost:
+                cost, best_route = new_travel_cost, route
+        return cost, best_route
+
+def what_time(method):
+    def timed(*args, **kwargs):
+        start_time = timeit.default_timer()
+        cost, best_route = method(*args, **kwargs)
+        return cost, best_route, timeit.default_timer() - start_time
 
 
-    def calculate_cost(self, symetric=True):
-        # import ipdb; ipdb.set_trace()
-        if symetric:
-            all_travels_costs = []
-            routes = itertools.permutations(list(range(self.number_of_cities)))
-            for route in routes:
-                travel_cost = 0
-                for i in range(0, len(route)-2):
-                    travel_cost += self.adjacency_matrix[route[i]][route[i+1]]
-                travel_cost += self.adjacency_matrix[route[len(route)-1]][0]
+tsp = Tsp('symetric', '14', True)
 
-                if travel_cost < self.lowest_cost:
-                    self.lowest_cost = travel_cost
-                    self.lowest_route = route
-                    print(route, ' --->',travel_cost)
-        else:
-            pass
-        print("===============BEST ROUTE=====================")
-        print(self.lowest_route, ' --->', self.lowest_cost)
-        return 1
-
-
-tsp = Tsp(file_dict['symetric']['14'])
-tsp.prepare_adjacency_matrix()
-tsp.display_matrix()
-tsp.calculate_cost()
